f8849f10877b987632d97a3d786cc0a467f99838
Kudera <34071009+kudera@users.noreply.github.com>
2018-03-03 16:09:07 -0800

    Update comped_accounts_updater.py

--- comped_accounts_updater.py
+++ comped_accounts_updater.py
@@ -2,11 +2,13 @@ import psycopg2
 import json
 import sys
 import requests
+import google_sheets_api_class
 
 
 with open("../config.json") as CONFIG:
     CONFIG = json.load(CONFIG)
 
+GOOGLE_SHEET = google_sheets_api_class.GoogleSheetsAPI("1fG60nPO-5fy0ovW8jpaafmB3t0LOt-SY5gfP_laKOLY")
 
 
 # Execute a SELECT query against mysql database with psycopg2
@@ -25,14 +27,22 @@ def query_mysql_db_psycopg2(query):
 
     return query_results
 
-    comped_account_query =  SELECT p.name, u.email, s.user_id, s.account_id, p.description, s.start_date              
-                            FROM mondo.subscriptions s     
-                            JOIN mondo.products p          
-                            ON s.product_id = p.id
-                            JOIN v3seomoz.users u
-                            ON u.id = s.user_id
-                            WHERE p.level LIKE '%pro%'     
-                            AND pay_processor = 'comped'
-                            AND NOT p.level LIKE '%student%'
-                              
 
+
+comped_account_query =  """SELECT p.name, u.email, s.user_id, s.account_id, p.description, s.start_date              
+                        FROM mondo.subscriptions s     
+                        JOIN mondo.products p          
+                        ON s.product_id = p.id
+                        JOIN v3seomoz.users u
+                        ON u.id = s.user_id
+                        WHERE p.level LIKE '%pro%'     
+                        AND pay_processor = 'comped'                            
+                        AND NOT p.level LIKE '%student%'""".format() #something needs to go here
+
+comped_accounts = [row for row in comped_account_query]
+
+comped_account_query_results = query_mysql_db_psycopg2(comped_account_query)
+print(comped_account_query_results)
+
+#comped_account_values = [list(row) for row in comped_account_query_results]
+#GOOGLE_SHEET.write_range("dave_test!A2", comped_accounts)
