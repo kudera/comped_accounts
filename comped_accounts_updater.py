5a4d643c10022ff4ec620b51807951628d72e590
Kudera <34071009+kudera@users.noreply.github.com>
2018-03-09 13:59:13 -0800

    Update comped_accounts_updater.py

--- comped_accounts_updater.py
+++ comped_accounts_updater.py
@@ -1,8 +1,9 @@
-import psycopg2
 import json
 import sys
 import requests
 import google_sheets_api_class
+import mysql.connector
+import datetime
 
 
 with open("../config.json") as CONFIG:
@@ -11,14 +12,12 @@ with open("../config.json") as CONFIG:
 GOOGLE_SHEET = google_sheets_api_class.GoogleSheetsAPI("1fG60nPO-5fy0ovW8jpaafmB3t0LOt-SY5gfP_laKOLY")
 
 
-# Execute a SELECT query against mysql database with psycopg2
-def query_mysql_db_psycopg2(query):
-    conn_string = "host='{0}' port={1} dbname='{2}' user='{3}' password='{4}'".format(CONFIG["sql"]["redshift"]["host"],
-                                                                                      CONFIG["sql"]["redshift"]["port"],
-                                                                                      CONFIG["sql"]["redshift"]["database"],
-                                                                                      CONFIG["sql"]["redshift"]["user"],
-                                                                                      CONFIG["sql"]["redshift"]["password"])
-    conn = psycopg2.connect(conn_string)
+# Execute a SELECT query against MySQL database
+def query_mysql_db(query):
+    conn = mysql.connector.connect(user = CONFIG["sql"]["v3seomoz"]["user"],
+                                   password = CONFIG["sql"]["v3seomoz"]["password"],
+                                   host = CONFIG["sql"]["v3seomoz"]["host"],
+                                   database = CONFIG["sql"]["v3seomoz"]["database"])
     cursor = conn.cursor()
     cursor.execute(query)
     query_results = cursor.fetchall()
@@ -28,21 +27,25 @@ def query_mysql_db_psycopg2(query):
     return query_results
 
 
+query_results = query_mysql_db("""SELECT p.name, u.email, s.user_id, s.account_id, p.description, s.start_date              
+                                  FROM mondo.subscriptions s     
+                                  JOIN mondo.products p          
+                                  ON s.product_id = p.id
+                                  JOIN v3seomoz.users u
+                                  ON u.id = s.user_id
+                                  WHERE p.level LIKE '%pro%'     
+                                  AND pay_processor = 'comped'                            
+                                  AND NOT p.level LIKE '%student%'""")
 
-comped_account_query =  """SELECT p.name, u.email, s.user_id, s.account_id, p.description, s.start_date              
-                        FROM mondo.subscriptions s     
-                        JOIN mondo.products p          
-                        ON s.product_id = p.id
-                        JOIN v3seomoz.users u
-                        ON u.id = s.user_id
-                        WHERE p.level LIKE '%pro%'     
-                        AND pay_processor = 'comped'                            
-                        AND NOT p.level LIKE '%student%'""".format() #something needs to go here
+query_values = [list(row) for row in query_results]
 
-comped_accounts = [row for row in comped_account_query]
+for row in query_values:
+    row[5] = row[5].strftime('%m/%d/%Y')
+    row.append(datetime.date.today().strftime('%m/%d/%Y'))
 
-comped_account_query_results = query_mysql_db_psycopg2(comped_account_query)
-print(comped_account_query_results)
 
-#comped_account_values = [list(row) for row in comped_account_query_results]
-#GOOGLE_SHEET.write_range("dave_test!A2", comped_accounts)
+header_list = [["NAME", "EMAIL", "USER ID", "ACCOUNT ID", "DESCRIPTION", "START DATE", "DATE RAN"]]
+
+GOOGLE_SHEET.insert_lines(GOOGLE_SHEET.sheet_ids["dave_test"], 2, len(query_values))
+GOOGLE_SHEET.write_range("dave_test!A1", header_list)
+GOOGLE_SHEET.write_range("dave_test!A2", query_values)
